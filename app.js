db5aabf9c8db5b79dacde79a5e9298a107c70ac7
baonhanthai2710 <baonhanthai2710@gmail.com>
2025-05-18 15:04:57 +0700

    ini

--- app.js
+++ app.js
@@ -323,3 +323,4 @@ app.use((err, req, res, next) => {
 // Server start
 //====================
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+//app.listen(PORT, '0.0.0.0', () => console.log(`Server running at http://0.0.0.0:${PORT}`));
